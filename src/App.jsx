1dbeddcab62c71fb7a5d9b44940f4f1e6b6aa390
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-18 17:33:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react"
+
+const App = () => {
+  const [users, setUsers] = useState([
+    {id:1, name:"Mark", age:16},
+    {id:2, name:"Stive", age:10},
+    {id:3, name:"Bob", age:19},
+    {id:4, name:"Alex", age:21},
+    {id:5, name:"Anna", age:20}
+  ])
+
+  const street = [
+     {id:1,name:"UK", userId:5},
+    {id:2,name:"USA", userId:4},
+    {id:3,name:"Ukrain", userId:3},
+    {id:4,name:"Russian", userId:2},
+    {id:5,name:"Korea", userId:1}
+  ]
+
+  const [userId, setUserId] = useState(0)
+
+  return (
+    <div className="flex flex-col justify-center">
+      <ul className="pt-20 pb-5 mx-auto ">
+        {users.map(item => (
+          <li className="text-[18px] font-medium " onClick={() => setUserId(item.id)}  key={item.id}>{item.name} id: {item.id} age: {item.age} </li>
+        ))}
+      </ul>
+        <hr />
+        <ul className="py-5 mx-auto text-center">
+          {street.filter(item => item.userId == userId).map(item => <p className=" text-[18px] font-normal " key={item.id}>Country: {item.name}</p>)}
+        </ul>
+
+    </div>
+  )
+}
+
+export default App
